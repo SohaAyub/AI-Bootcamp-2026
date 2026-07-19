e9dd1885f3cde08a0ea1216603ec166a5dca7e57
Soha Ayub <sohaayub80@gmail.com>
2026-07-19 13:27:08 +0500

    Create favorite_food.py
    This Python program collects information about the user's dream career,
    preferred company, and desired work location. The entered information is
    displayed using formatted output.
    
    The exercise demonstrates how Python applications can interact with users,
    collect meaningful information, and organize it using variables.

---
+++ Week-01/Day-01/favorite_food.py
@@ -0,0 +1,10 @@
+# Favorite Food Program
+
+food = input("What is your favorite food? ")
+drink = input("What is your favorite drink? ")
+dessert = input("What is your favorite dessert? ")
+
+print("\n===== Favorite Food =====")
+print("Food:", food)
+print("Drink:", drink)
+print("Dessert:", dessert)
