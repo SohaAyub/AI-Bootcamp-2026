4a2ad2898c7e1907d30b8524e4b3412e55f59bda
Soha Ayub <sohaayub80@gmail.com>
2026-07-20 20:18:31 +0500

    Create comparison_operators.py

---
+++ Week-01/Day-02/comparison_operators.py
@@ -0,0 +1,35 @@
+"""
+Project: Comparison Operators
+Week: 01
+Day: 02
+
+Description:
+This program compares two numbers using comparison operators and
+returns True or False based on the comparison.
+
+Concepts Covered:
+- Comparison Operators
+- Boolean Values
+- Variables
+
+Learning Objective:
+To understand how Python compares values using relational operators.
+
+Author: Soha Ayub
+Repository: AI-Bootcamp-2026
+"""
+
+print("=" * 45)
+print("      COMPARISON OPERATORS")
+print("=" * 45)
+
+num1 = int(input("Enter First Number: "))
+num2 = int(input("Enter Second Number: "))
+
+print("\nResults")
+print("Equal:", num1 == num2)
+print("Not Equal:", num1 != num2)
+print("Greater Than:", num1 > num2)
+print("Less Than:", num1 < num2)
+print("Greater or Equal:", num1 >= num2)
+print("Less or Equal:", num1 <= num2)
