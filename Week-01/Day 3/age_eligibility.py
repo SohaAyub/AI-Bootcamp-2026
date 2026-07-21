5f84b62817b2a53ac914b2ddb84652befa200c2a
Soha Ayub <sohaayub80@gmail.com>
2026-07-21 17:56:18 +0500

    Create age_eligibility.py

---
+++ Week-01/Day 3/age_eligibility.py
@@ -0,0 +1,18 @@
+"""
+Project: Age Eligibility Checker
+Week: 01
+Day: 03
+
+Description:
+Determines whether a user is eligible to vote based on age.
+
+Author: Soha Ayub
+Repository: AI-Bootcamp-2026
+"""
+
+age = int(input("Enter your age: "))
+
+if age >= 18:
+    print("You are eligible to vote.")
+else:
+    print("You are not eligible to vote.")
