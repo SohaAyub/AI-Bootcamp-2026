c39db44dd45003d9153933833d3fbb6fdcaef57f
Soha Ayub <sohaayub80@gmail.com>
2026-07-22 17:05:19 +0500

    Create sum_of_numbers.py

---
+++ Week-01/Day-4/sum_of_numbers.py
@@ -0,0 +1,26 @@
+"""
+Project: Sum of Numbers
+Week: 01
+Day: 04
+
+Description:
+This program calculates the sum of numbers from 1 to 100.
+
+Concepts Covered:
+- for loop
+- Variables
+- Arithmetic Operators
+
+Learning Objective:
+To understand accumulation using loops.
+
+Author: Soha Ayub
+Repository: AI-Bootcamp-2026
+"""
+
+total = 0
+
+for i in range(1, 101):
+    total += i
+
+print("Sum of numbers from 1 to 100 is:", total)
