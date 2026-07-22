63485acaf994cad5ab8c2bd19d894623e0ce6cf6
Soha Ayub <sohaayub80@gmail.com>
2026-07-22 17:12:43 +0500

    Create *_ pattern.py

---
+++ Week-01/Day-4/*_ pattern.py
@@ -0,0 +1,21 @@
+"""
+Project: Star Pattern
+Week: 01
+Day: 04
+
+Description:
+This program prints a simple star pattern using nested loops.
+
+Concepts Covered:
+- Nested for loops
+- String Multiplication
+
+Learning Objective:
+To understand how nested loops work by creating patterns.
+
+Author: Soha Ayub
+Repository: AI-Bootcamp-2026
+"""
+
+for i in range(1, 6):
+    print("*" * i)
