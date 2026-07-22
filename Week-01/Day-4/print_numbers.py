9da62175988f2e75cf8b49be7c2a878408844d13
Soha Ayub <sohaayub80@gmail.com>
2026-07-22 17:02:31 +0500

    Create  print_numbers.py

---
+++ Week-01/Day-4/print_numbers.py
@@ -0,0 +1,23 @@
+"""
+Project: Print Numbers
+Week: 01
+Day: 04
+
+Description:
+This program prints numbers from 1 to 10 using a for loop.
+
+Concepts Covered:
+- for loop
+- range() function
+
+Learning Objective:
+To understand how a for loop repeats code a specific number of times.
+
+Author: Soha Ayub
+Repository: AI-Bootcamp-2026
+"""
+
+print("Numbers from 1 to 10:\n")
+
+for i in range(1, 11):
+    print(i)
