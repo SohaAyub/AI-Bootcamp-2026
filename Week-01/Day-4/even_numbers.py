7d83d4c3079d0243a71156cadc1f275835f0bbd3
Soha Ayub <sohaayub80@gmail.com>
2026-07-22 17:04:09 +0500

    Create even_numbers.py

---
+++ Week-01/Day-4/even_numbers.py
@@ -0,0 +1,24 @@
+"""
+Project: Even Numbers
+Week: 01
+Day: 04
+
+Description:
+This program prints even numbers from 2 to 20 using a for loop.
+
+Concepts Covered:
+- for loop
+- range()
+- Step value
+
+Learning Objective:
+To understand how range() can skip numbers using a step value.
+
+Author: Soha Ayub
+Repository: AI-Bootcamp-2026
+"""
+
+print("Even Numbers from 2 to 20:\n")
+
+for i in range(2, 21, 2):
+    print(i)
