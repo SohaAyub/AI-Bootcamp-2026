0584578d7d72c600fa58a5ec82958aadff66786d
Soha Ayub <sohaayub80@gmail.com>
2026-07-22 17:06:39 +0500

    Create countdown.py

---
+++ Week-01/Day-4/countdown.py
@@ -0,0 +1,26 @@
+"""
+Project: Countdown Timer
+Week: 01
+Day: 04
+
+Description:
+This program counts down from 10 to 1 using a while loop.
+
+Concepts Covered:
+- while loop
+- Variables
+
+Learning Objective:
+To understand repetition using while loops.
+
+Author: Soha Ayub
+Repository: AI-Bootcamp-2026
+"""
+
+count = 10
+
+while count >= 1:
+    print(count)
+    count -= 1
+
+print("Time's Up!")
