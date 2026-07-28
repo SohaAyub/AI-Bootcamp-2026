668460f2e21d83e3049272d4ddcafa8bc4627c1c
Soha Ayub <sohaayub80@gmail.com>
2026-07-28 17:31:38 +0500

    Create shopping_list.py

---
+++ Week-01/Day-06/shopping_list.py
@@ -0,0 +1,21 @@
+"""
+Project: Shopping List Manager
+Week: 01
+Day: 06
+
+Description:
+Demonstrates adding and removing items from a list.
+
+Author: Soha Ayub
+Repository: AI-Bootcamp-2026
+"""
+
+shopping_list = ["Milk", "Bread", "Eggs"]
+
+print("Original List:", shopping_list)
+
+shopping_list.append("Butter")
+print("After Adding Butter:", shopping_list)
+
+shopping_list.remove("Bread")
+print("After Removing Bread:", shopping_list)
