b56a1bbac402b501022bc5a2dc331608406c322c
Soha Ayub <sohaayub80@gmail.com>
2026-07-28 17:34:59 +0500

    Add student grade list analyzer script
    This script analyzes a list of student grades, providing statistics such as highest, lowest, total, and average grades.

---
+++ Week-01/Day-06/student_grade_list.py
@@ -0,0 +1,19 @@
+"""
+Project: Student Grade List Analyzer
+Week: 01
+Day: 06
+
+Description:
+Analyzes a list of student grades.
+
+Author: Soha Ayub
+Repository: AI-Bootcamp-2026
+"""
+
+grades = [78, 85, 92, 67, 88, 95]
+
+print("Student Grades:", grades)
+print("Highest Grade:", max(grades))
+print("Lowest Grade:", min(grades))
+print("Total Students:", len(grades))
+print("Average Grade:", sum(grades) / len(grades))
