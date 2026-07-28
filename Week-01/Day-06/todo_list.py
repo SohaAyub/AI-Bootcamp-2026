8f83860cec0af1aea708fb39702040057b490fe6
Soha Ayub <sohaayub80@gmail.com>
2026-07-28 17:32:16 +0500

    Create todo_list.py

---
+++ Week-01/Day-06/todo_list.py
@@ -0,0 +1,21 @@
+"""
+Project: To-Do List
+Week: 01
+Day: 06
+
+Description:
+Creates and displays a simple to-do list.
+
+Author: Soha Ayub
+Repository: AI-Bootcamp-2026
+"""
+
+tasks = []
+
+tasks.append("Learn Python")
+tasks.append("Practice Coding")
+tasks.append("Upload to GitHub")
+
+print("My To-Do List:")
+for task in tasks:
+    print("-", task)
