ab742281251f4f0c987b02e85f2b931635a79dda
Soha Ayub <sohaayub80@gmail.com>
2026-07-28 17:33:11 +0500

    Create favorite_movies.py

---
+++ Week-01/Day-06/favorite_movies.py
@@ -0,0 +1,18 @@
+"""
+Project: Favorite Movies List
+Week: 01
+Day: 06
+
+Description:
+Stores favorite movies and demonstrates indexing and slicing.
+
+Author: Soha Ayub
+Repository: AI-Bootcamp-2026
+"""
+
+movies = ["Interstellar", "Inception", "The Dark Knight", "Avatar"]
+
+print("All Movies:", movies)
+print("First Movie:", movies[0])
+print("Last Movie:", movies[-1])
+print("First Two Movies:", movies[0:2])
